39d4c9cb0fd7cb4859afbf5204bf1929f05ccb0b
Nurik <yanzip@yandex.ru>
2024-06-03 12:32:55 +0500

    добавлен в код подрообное комментарии

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,31 @@
+
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector("#card-template").content;
+const cardTemplate = document.querySelector("#card-template").content; // Получаем шаблон карточки из HTML-документа
 
 // @todo: DOM узлы
-const cardList = document.querySelector(".places__list");
+const cardList = document.querySelector(".places__list"); // Получаем список, в который будем добавлять карточки
 
 // @todo: Функция создания карточки
-function addNewCard(item, deleteCard) {
-  const cardListItem = cardTemplate.querySelector('.places__item').cloneNode(true);
-  const cardImage = cardListItem.querySelector('.card__image');
-  cardImage.src = item.link;
-  cardImage.alt = item.name;
-  cardListItem.querySelector('.card__title').textContent = item.name;
-  const deleteButton = cardListItem.querySelector(".card__delete-button");
+function addNewCard(item, deleteCard) { // Функция для создания новой карточки
+  const cardListItem = cardTemplate.querySelector('.places__item').cloneNode(true); // Клонируем элемент карточки из шаблона
+  const cardImage = cardListItem.querySelector('.card__image'); // Находим элемент изображения в карточке
+  cardImage.src = item.link; // Устанавливаем ссылку на изображение
+  cardImage.alt = item.name; // Устанавливаем альтернативный текст для изображения
+  cardListItem.querySelector('.card__title').textContent = item.name;  // Устанавливаем заголовок карточки
+  const deleteButton = cardListItem.querySelector(".card__delete-button"); // Находим кнопку удаления и добавляем обработчик события
   deleteButton.addEventListener('click', deleteCard);
-  return cardListItem;
+  return cardListItem; // Возвращаем созданную карточку
 }
 
 // @todo: Функция удаления карточки
-function deleteCard(evt) {
-  evt.target.closest(".card").remove();
+function deleteCard(evt) { // Функция для удаления карточки
+  evt.target.closest(".card").remove(); // Удаляем ближайшую родительскую карточку
 }
 
 // @todo: Вывести карточки на страницу
-initialCards.forEach(function(item) {
+initialCards.forEach(function (item) { // Создаем карточки из начального массива и добавляем их в список
   const cardElement = addNewCard(item, deleteCard);
   cardList.append(cardElement);
 });
-  
 
 
