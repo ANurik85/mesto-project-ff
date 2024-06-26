f650afb26dc2ecc6063aff657c3fd35481488633
Nurik <yanzip@yandex.ru>
2024-06-26 17:30:06 +0500

    проект разделён по модулями

---
+++ src/components/card.js
@@ -0,0 +1,27 @@
+/*
+Функции для работы с карточками проекта Mesto вынесите в файл card.js, из него должна экспортироваться функция createCard, которую вы создали раньше (у вас она может называться по-другому). Функции, обрабатывающие события лайка и удаления карточки, также должны находиться в этом файле и экспортироваться из него.
+
+!!! Чтобы было чуточку понятнее: вызов функции создания карточки должен находиться в файле index.js, но само объявление функции — в card.js. 
+*/
+
+import { cardTemplate } from '../scripts/index.js';
+import { openModal, closeModal } from '../components/modal.js';
+
+// @todo: Функция создания карточки
+export function addNewCard(item, deleteCard, openModal) { // Функция для создания новой карточки
+  const cardListItem = cardTemplate.querySelector('.places__item').cloneNode(true); // Клонируем элемент карточки из шаблона
+  const cardImage = cardListItem.querySelector('.card__image'); // Находим элемент изображения в карточке
+  cardImage.src = item.link; // Устанавливаем ссылку на изображение
+  cardImage.alt = item.name; // Устанавливаем альтернативный текст для изображения
+  cardListItem.querySelector('.card__title').textContent = item.name;  // Устанавливаем заголовок карточки
+
+  const deleteButton = cardListItem.querySelector(".card__delete-button"); // Находим кнопку удаления и добавляем обработчик события
+  deleteButton.addEventListener('click', deleteCard);
+
+  return cardListItem; // Возвращаем созданную карточку
+}
+
+// @todo: Функция удаления карточки
+export function deleteCard(evt) { // Функция для удаления карточки
+  evt.target.closest(".card").remove(); // Удаляем ближайшую родительскую карточку
+}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,35 +1,36 @@
- 
+/*
+В файле index.js должны остаться:
+1. объявления и инициализация глобальных констант и переменных с DOM-элементами страницы,
+2. обработчики событий (при открытии и закрытии попапов; при отправке форм; обработчик, открывающий попап при клике по изображению карточки);
+3. вызовы других функций, подключённых из созданных модулей, которым нужно будет передавать объявленные здесь переменные и обработчики.
+
+!!! Чтобы было чуточку понятнее: вызов функции создания карточки должен находиться в файле index.js, но само объявление функции — в card.js. 
+*/ 
+
+import '../pages/index.css'; // добавьте импорт главного файла стилей 
 import { initialCards } from './cards.js';
- import '../pages/index.css'; // добавьте импорт главного файла стилей 
+import { addNewCard, deleteCard } from '../components/card.js';
+import { openModal, closeModal } from '../components/modal.js';
+
 
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector("#card-template").content; // Получаем шаблон карточки из HTML-документа
-
+export const cardTemplate = document.querySelector("#card-template").content; // Получаем шаблон карточки из HTML-документа
 // @todo: DOM узлы
 const cardList = document.querySelector(".places__list"); // Получаем список, в который будем добавлять карточки
 
-// @todo: Функция создания карточки
-function addNewCard(item, deleteCard) { // Функция для создания новой карточки
-  const cardListItem = cardTemplate.querySelector('.places__item').cloneNode(true); // Клонируем элемент карточки из шаблона
-  const cardImage = cardListItem.querySelector('.card__image'); // Находим элемент изображения в карточке
-  cardImage.src = item.link; // Устанавливаем ссылку на изображение
-  cardImage.alt = item.name; // Устанавливаем альтернативный текст для изображения
-  cardListItem.querySelector('.card__title').textContent = item.name;  // Устанавливаем заголовок карточки
-  const deleteButton = cardListItem.querySelector(".card__delete-button"); // Находим кнопку удаления и добавляем обработчик события
-  deleteButton.addEventListener('click', deleteCard);
-  return cardListItem; // Возвращаем созданную карточку
-}
+const openButton = document.querySelector('.button');
+const modal = document.querySelectorAll('.popup');
+const closeButton = document.querySelector('.popup__close');
 
-// @todo: Функция удаления карточки
-function deleteCard(evt) { // Функция для удаления карточки
+
+openButton.addEventListener('click', openModal);
+closeButton.addEventListener('click', closeModal);
   
-  evt.target.closest(".card").remove(); // Удаляем ближайшую родительскую карточку
-}
 
 // @todo: Вывести карточки на страницу
 // Создаем карточки из начального массива и добавляем их в список
 initialCards.forEach(function (item) {
-  const cardElement = addNewCard(item, deleteCard);
+  const cardElement = addNewCard(item, deleteCard, openModal);
   cardList.append(cardElement);
 });
 
